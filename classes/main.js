6520cd2beade3332d4a0b7d4190fa43f64791e39
bzaPetey <peter.laliberte@burgzergarcde.net>
2023-09-20 05:08:31 -0400

    separate classes done.

--- classes/main.js
+++ classes/main.js
@@ -1,24 +1,17 @@
 import * as BABYLON from "@babylonjs/core";
-import HavokPhysics from "@babylonjs/havok";
+import { EvilCubie } from "./EvilCubie";
 
 const canvas = document.getElementById("renderCanvas");
 
 const engine = new BABYLON.WebGPUEngine(canvas);
 await engine.initAsync();
 
-const HK = await HavokPhysics();
-
 const scene = new BABYLON.Scene(engine);
-
-scene.enablePhysics(
-  new BABYLON.Vector3(0, -9.81, 0),
-  new BABYLON.HavokPlugin()
-);
-
 scene.clearColor = new BABYLON.Color3(0.05, 0.05, 0.15);
 scene.createDefaultCameraOrLight(true, false, true);
 
-const box = BABYLON.MeshBuilder.CreateBox("box", { size: 0.1 }, scene);
+const box = new EvilCubie("Evil Dude");
+//box.init();
 
 //render the scene every frame.
 engine.runRenderLoop(function () {
--- gravity/main2.js
+++ gravity/main2.js
@@ -1,20 +1,11 @@
 import * as BABYLON from "@babylonjs/core";
-import HavokPhysics from "@babylonjs/havok";
 
 const canvas = document.getElementById("renderCanvas");
 
 const engine = new BABYLON.WebGPUEngine(canvas);
 await engine.initAsync();
 
-const HK = await HavokPhysics();
-
 const scene = new BABYLON.Scene(engine);
-
-scene.enablePhysics(
-  new BABYLON.Vector3(0, -9.81, 0),
-  new BABYLON.HavokPlugin()
-);
-
 scene.clearColor = new BABYLON.Color3(0.05, 0.05, 0.15);
 scene.createDefaultCameraOrLight(true, false, true);
 
